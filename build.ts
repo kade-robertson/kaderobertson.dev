6b7b120be8e6d8d48498eeaf9d7e01bc555c65e7
Kade Robertson <kade@kaderobertson.dev>
2022-08-17 09:30:16 -0400

    build: use critical to inline css

--- build.ts
+++ build.ts
@@ -2,23 +2,33 @@
 import HtmlWebpackInlineSVGPlugin from "html-webpack-inline-svg-plugin";
 import fs from "fs-extra";
 
-const main = async () => {
-  // Copy files from 'src' to 'dist'.
-  try {
-    if ((await fs.stat("dist")).isDirectory()) {
-      await fs.rm("dist", { recursive: true, force: true });
-    }
-  } catch {
-    // 'dist' doesn't exist, proceed as normal.
-  } finally {
-    await fs.copy("src", "dist");
+// @ts-ignore
+import critical from "critical";
+
+// Copy files from 'src' to 'dist'.
+try {
+  if ((await fs.stat("dist")).isDirectory()) {
+    await fs.rm("dist", { recursive: true, force: true });
   }
+} catch {
+  // 'dist' doesn't exist, proceed as normal.
+} finally {
+  await fs.copy("src", "dist");
+}
 
-  // Parse for inline SVGs & rewrite
-  const htmlData = await fs.readFile("dist/index.html", { encoding: "utf-8" });
-  const svgInliner = new HtmlWebpackInlineSVGPlugin();
-  const fixedHtmlData = await svgInliner.processImages(htmlData);
-  await fs.writeFile("dist/index.html", fixedHtmlData);
-};
+// Parse for inline SVGs & rewrite
+const htmlData = await fs.readFile("dist/index.html", { encoding: "utf-8" });
 
-main();
+const svgInliner = new HtmlWebpackInlineSVGPlugin();
+const svgInlinedHtmlData = await svgInliner.processImages(htmlData);
+
+const { html: inlinedCssHtmlData } = await critical.generate({
+  inline: true,
+  base: "dist/",
+  html: svgInlinedHtmlData,
+  extract: true,
+  width: 1300,
+  height: 900,
+});
+
+await fs.writeFile("dist/index.html", inlinedCssHtmlData);
