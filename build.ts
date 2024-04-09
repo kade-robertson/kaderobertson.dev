c791418c91c919f975a38e748fb62fe41a424c00
Kade <kade-robertson@users.noreply.github.com>
2024-04-09 16:32:06 -0400

    chore: simplify deps and build (#24)

--- build.ts
+++ build.ts
@@ -1,6 +1,5 @@
-import { generate } from "critical";
-import HtmlWebpackInlineSVGPlugin from "html-webpack-inline-svg-plugin";
 import fs from "fs-extra";
+import { inlineSource } from "inline-source";
 
 // Copy files from 'src' to 'dist'.
 try {
@@ -13,19 +12,4 @@ try {
   await fs.copy("src", "dist");
 }
 
-// Parse for inline SVGs & rewrite
-const htmlData = await fs.readFile("dist/index.html", { encoding: "utf-8" });
-
-const svgInliner = new HtmlWebpackInlineSVGPlugin();
-const svgInlinedHtmlData = await svgInliner.processImages(htmlData);
-
-const { html: inlinedCssHtmlData } = await generate({
-  inline: true,
-  base: "dist/",
-  html: svgInlinedHtmlData,
-  extract: true,
-  width: 1300,
-  height: 900,
-});
-
-await fs.writeFile("dist/index.html", inlinedCssHtmlData);
+await fs.writeFile("dist/index.html", await inlineSource("dist/index.html"));
